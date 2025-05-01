fa670cf80f8d11d494ece1c7781965f07a4396d3
alikhan khamidulla <khamidulla.alikhan@gmail.com>
2025-05-01 12:18:26 +0500

    KNN with FRONT

--- python_back/main.py
+++ python_back/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
 from routes.classification_router import router as reg_route
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(reg_route)
